219abe7dae6f5bfe954f9398ac77f1d4e2dd7e39
earsuit <renyudicn@outlook.com>
2023-12-06 19:39:57 +0800

    add: basic ui framwork

--- src/ui/HistoricalMap.cpp
+++ src/ui/HistoricalMap.cpp
@@ -1,12 +1,5 @@
 #include "src/ui/HistoricalMap.h"
 
-#include "external/imgui/imgui.h"
-#include "external/imgui/backends/imgui_impl_glfw.h"
-#include "external/imgui/backends/imgui_impl_opengl3.h"
-
-#include <GL/glew.h>
-#include <GLFW/glfw3.h>
-
 #include <stdexcept>
 #include <string>
 
@@ -20,14 +13,69 @@ static void glfw_error_callback(int error, const char* description)
 }
 }
 
-bool HistoricalMap::prepare()
+HistoricalMap::HistoricalMap()
 {
     glfwSetErrorCallback(glfw_error_callback);
     if (!glfwInit()) {
-        return false;
+        throw std::runtime_error("Failed to initialize glfw.");
     }
     
+    const auto glsl_version = "#version 130";
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    // Create window with graphics context
+    window = glfwCreateWindow(1280, 720, "Historical Map", NULL, NULL);
+    if (window == NULL) {
+        throw std::runtime_error("Failed to create window.");
+    }
+
+    glfwMakeContextCurrent(window);
+    glfwSwapInterval(1); // Enable vsync
+
+    if (glewInit()) {
+        throw std::runtime_error("Failed to initialize OpenGL loader!");
+    }
     
+    // Setup Dear ImGui context
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGui::StyleColorsDark();
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+}
+
+HistoricalMap::~HistoricalMap()
+{
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    glfwDestroyWindow(window);
+    glfwTerminate();
+}
+
+void HistoricalMap::start()
+{
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        ImGui::Render();
+        int display_w, display_h;
+        glfwGetFramebufferSize(window, &display_w, &display_h);
+        glViewport(0, 0, display_w, display_h);
+        glClearColor(clear_color.x, clear_color.y, clear_color.z, clear_color.w);
+        glClear(GL_COLOR_BUFFER_BIT);
+        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+        glfwSwapBuffers(window);
+    }
 }
 
 }
--- src/ui/HistoricalMap.h
+++ src/ui/HistoricalMap.h
@@ -1,15 +1,25 @@
 #ifndef SRC_UI_HISTORICAL_MAP_H
 #define SRC_UI_HISTORICAL_MAP_H
 
+#include "external/imgui/imgui.h"
+#include "external/imgui/backends/imgui_impl_glfw.h"
+#include "external/imgui/backends/imgui_impl_opengl3.h"
+
+#include <GL/glew.h>
+#include <GLFW/glfw3.h>
+
 namespace ui {
 
 class HistoricalMap {
 public:
-    bool start();
+    HistoricalMap();
+    ~HistoricalMap();
+
+    void start();
 
 private:
-    bool prepare();
-    void mainLoop();
+    GLFWwindow* window;
+    ImVec4 clear_color;
 };
 
 }
