e1fb1e5538799b07403a64dfd32a6c5fd504d9bf
earsuit <renyudicn@outlook.com>
2023-12-19 20:13:09 +0800

    add: logger to MapWidget

--- src/ui/MapWidget.h
+++ src/ui/MapWidget.h
@@ -2,6 +2,7 @@
 #define SRC_UI_MAP_PLOT_H
 
 #include "external/imgui/imgui.h"
+#include "spdlog/spdlog.h"
 
 #include <utility>
 
@@ -9,9 +10,12 @@ namespace ui {
 
 class MapWidget {
 public:
+    MapWidget(spdlog::logger& logger): logger{logger} {}
     void paint();
 
 private:
+    spdlog::logger& logger;
+
     std::pair<ImVec2, ImVec2> calculateBound(int x, int y);
 };
 }
