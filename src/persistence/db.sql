8f0616211600ac83aad668336dcf335f8d97256a
earsuit <renyudicn@outlook.com>
2023-12-28 19:55:46 +0800

    rf: make table more clear

--- src/persistence/db.sql
+++ src/persistence/db.sql
@@ -1,9 +1,33 @@
-CREATE TABLE IF NOT EXISTS years (
-    year INTEGER PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS relationship (
+    id INTEGER PRIMARY KEY,
+    year_id INTEGER NOT NULL,
     country_id INTEGER NOT NULL,
     border_id INTEGER NOT NULL,
     FOREIGN KEY (country_id) REFERENCES countries(id),
     FOREIGN KEY (border_id) REFERENCES borders(id)
+    FOREIGN KEY (year_id) REFERENCES years(id)
+);
+
+/* Cities may not belong to any country*/
+CREATE TABLE IF NOT EXISTS cities (
+    id INTEGER PRIMARY KEY,
+    name TEXT NOT NULL,
+    year_id INTEGER NOT NULL,
+    latitude real NOT NULL,
+    longitude real NOT NULL,
+    FOREIGN KEY (year_id) REFERENCES years(id)
+);
+
+CREATE TABLE IF NOT EXISTS events (
+    id INTEGER PRIMARY KEY,
+    year_id INTEGER NOT NULL,
+    event TEXT NOT NULL,
+    FOREIGN KEY (year_id) REFERENCES years(id)
+);
+
+CREATE TABLE IF NOT EXISTS years (
+    id INTEGER PRIMARY KEY,
+    year INTEGER NOT NULL,
 );
 
 CREATE TABLE IF NOT EXISTS countries (
@@ -14,20 +38,4 @@ CREATE TABLE IF NOT EXISTS countries (
 CREATE TABLE IF NOT EXISTS borders (
     id INTEGER PRIMARY KEY,
     contour blob NOT NULL  -- A list of (latitude, longitude) defines the contour of the country
-);
-
-CREATE TABLE IF NOT EXISTS cities (
-    id INTEGER PRIMARY KEY,
-    name TEXT NOT NULL,
-    year INTEGER NOT NULL,
-    country_id INTEGER NOT NULL,
-    FOREIGN KEY (year) REFERENCES years(year),
-    FOREIGN KEY (country_id) REFERENCES countries(id)
-);
-
-CREATE TABLE IF NOT EXISTS events (
-    id INTEGER PRIMARY KEY,
-    event TEXT NOT NULL,
-    year INTEGER NOT NULL,
-    FOREIGN KEY (year) REFERENCES years(year)
 );
