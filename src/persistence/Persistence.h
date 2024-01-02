f36e83c85cc1684b5c8cff14603d7f25a0494685
earsuit <renyudicn@outlook.com>
2024-01-02 23:03:19 +0800

    fix: get wrong data from blob
    The blob is destroyed if use a reference to the blob directly at the same line when get the rows,
    and blob is a pointer, so the real memory is released.
    
    db(select().from().where()).front().name

--- src/persistence/Persistence.h
+++ src/persistence/Persistence.h
@@ -56,8 +56,10 @@ public:
             auto requestCountriesTask = launchAsync([this, yearId](){
                 std::vector<Country> countries;
                 for (const auto& row : request<table::Relationships>(RELATIONSHIPS.yearId == yearId)) {
-                    const auto& countryName = this->request<table::Countries>(COUNTRIES.id == row.countryId).front().name;
-                    const auto& borderContour = this->request<table::Borders>(BORDERS.id == row.borderId).front().contour;
+                    const auto countryName = this->request<table::Countries>(COUNTRIES.id == row.countryId).front().name;
+
+                    const auto& borderRows = this->request<table::Borders>(BORDERS.id == row.borderId);
+                    const auto& borderContour = borderRows.front().contour;
                     const auto& contour = deserializeContour(std::vector<uint8_t>{borderContour.blob, borderContour.blob + borderContour.len});
 
                     countries.emplace_back(countryName, contour);
@@ -66,16 +68,21 @@ public:
                 return countries;
             });
 
+            data.countries = requestCountriesTask.get();
+
             auto requestCitiesTask = launchAsync([this, yearId](){
                 std::vector<City> cities;
                 for (const auto& row : this->request<table::YearCities>(YEAR_CITIES.yearId == yearId)) {
-                    const auto& city = this->request<table::Cities>(CITIES.id == row.cityId).front();
+                    const auto& cityRows = this->request<table::Cities>(CITIES.id == row.cityId);
+                    const auto& city = cityRows.front();
 
                     cities.emplace_back(city.name, Coordinate{city.latitude, city.longitude});
                 }
 
                 return cities;
             });
+
+            data.cities = requestCitiesTask.get();
 
             auto requestEventTask = launchAsync([this, yearId]() -> std::optional<Event> {
                 if (const auto& ret = this->request<table::Events>(EVENTS.yearId == yearId); !ret.empty()) {
@@ -85,8 +92,6 @@ public:
                 }
             });
 
-            data.countries = requestCountriesTask.get();
-            data.cities = requestCitiesTask.get();
             data.event = requestEventTask.get();
         }
 
@@ -95,55 +100,58 @@ public:
 
     void upsert(const Data& data)
     {
+        uint64_t yearId = 0;
         if (const auto& years = request<table::Years>(YEARS.year == data.year); !years.empty()) {
-            const auto yearId = years.front().id;
-
-            auto upsertCountriesTask = launchAsync([this, yearId, &countries = data.countries](){
-                for (const auto& country : countries) {
-                    const auto& serializedContour = serializeContour(country.borderContour);
-
-                    if (const auto& countryIdOpt = this->upsert<table::Countries>(COUNTRIES.name == country.name, COUNTRIES.name = country.name); countryIdOpt) {
-                        // country doesn't exist in the table before, it means the contour doesn't exist either
-                        const auto borderId = this->insert<table::Borders>(BORDERS.contour = serializedContour);
-
-                        this->insert<table::Relationships>(RELATIONSHIPS.yearId = yearId, RELATIONSHIPS.countryId = *countryIdOpt, RELATIONSHIPS.borderId = borderId);
-                    } else {
-                        // country already exists in the table, so as the border
-                        const auto countryId = this->request<table::Countries>(COUNTRIES.name == country.name).front().id;
-                        const auto borderId = this->request<table::Relationships>(RELATIONSHIPS.yearId == yearId && RELATIONSHIPS.countryId == countryId).front().id;
-
-                        this->update<table::Borders>(BORDERS.id == borderId, BORDERS.contour = serializedContour);
-                    }
-                }
-            });
-
-            auto upsertCitiesTask = launchAsync([this, yearId, &cities = data.cities](){
-                for (const auto& city : cities) {
-                    uint64_t cityId = 0;
-                    if (const auto cityIdOpt = this->upsert<table::Cities>(CITIES.name == city.name, 
-                                                                        CITIES.name = city.name, 
-                                                                        CITIES.latitude = city.coordinate.latitude,
-                                                                        CITIES.longitude = city.coordinate.longitude); cityIdOpt) {
-                        cityId = *cityIdOpt;
-                    } else {
-                        cityId = this->request<table::Cities>(CITIES.name == city.name).front().id;
-                    }
-
-                    this->upsert<table::YearCities>(YEAR_CITIES.yearId == yearId && YEAR_CITIES.cityId == cityId, 
-                                                    YEAR_CITIES.yearId = yearId, YEAR_CITIES.cityId = cityId);
-                }
-            });
-
-            auto upsertEventTask = launchAsync([this, yearId, &event = data.event](){
-                if (event) {
-                    this->upsert<table::Events>(EVENTS.yearId == yearId, EVENTS.yearId = yearId, EVENTS.event = event->description);
-                }
-            });
-
-            upsertCountriesTask.wait();
-            upsertCitiesTask.wait();
-            upsertEventTask.wait();
+            yearId = years.front().id;
+        } else {
+            yearId = insert<table::Years>(YEARS.year = data.year);
         }
+
+        auto upsertCountriesTask = launchAsync([this, yearId, &countries = data.countries](){
+            for (const auto& country : countries) {
+                const auto& serializedContour = serializeContour(country.borderContour);
+
+                if (const auto& countryIdOpt = this->upsert<table::Countries>(COUNTRIES.name == country.name, COUNTRIES.name = country.name); countryIdOpt) {
+                    // country doesn't exist in the table before, it means the contour doesn't exist either
+                    const auto borderId = this->insert<table::Borders>(BORDERS.contour = serializedContour);
+
+                    this->insert<table::Relationships>(RELATIONSHIPS.yearId = yearId, RELATIONSHIPS.countryId = *countryIdOpt, RELATIONSHIPS.borderId = borderId);
+                } else {
+                    // country already exists in the table, so as the border
+                    const uint64_t countryId = this->request<table::Countries>(COUNTRIES.name == country.name).front().id;
+                    const uint64_t borderId = this->request<table::Relationships>(RELATIONSHIPS.yearId == yearId && RELATIONSHIPS.countryId == countryId).front().id;
+
+                    this->update<table::Borders>(BORDERS.id == borderId, BORDERS.contour = serializedContour);
+                }
+            }
+        });
+
+        auto upsertCitiesTask = launchAsync([this, yearId, &cities = data.cities](){
+            for (const auto& city : cities) {
+                uint64_t cityId = 0;
+                if (const auto cityIdOpt = this->upsert<table::Cities>(CITIES.name == city.name, 
+                                                                    CITIES.name = city.name, 
+                                                                    CITIES.latitude = city.coordinate.latitude,
+                                                                    CITIES.longitude = city.coordinate.longitude); cityIdOpt) {
+                    cityId = *cityIdOpt;
+                } else {
+                    cityId = this->request<table::Cities>(CITIES.name == city.name).front().id;
+                }
+
+                this->upsert<table::YearCities>(YEAR_CITIES.yearId == yearId && YEAR_CITIES.cityId == cityId, 
+                                                YEAR_CITIES.yearId = yearId, YEAR_CITIES.cityId = cityId);
+            }
+        });
+
+        auto upsertEventTask = launchAsync([this, yearId, &event = data.event](){
+            if (event) {
+                this->upsert<table::Events>(EVENTS.yearId == yearId, EVENTS.yearId = yearId, EVENTS.event = event->description);
+            }
+        });
+
+        upsertCountriesTask.wait();
+        upsertCitiesTask.wait();
+        upsertEventTask.wait();
     }
 
 private:
