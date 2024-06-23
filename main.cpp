887f3bbdae1e8172a5522bbf967448c881fa100b
Earsuit <renyudicn@outlook.com>
2024-06-23 11:57:18 +0800

    fix: disable console window on Windows

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include "src/ui/HistoricalMap.h"
 
+#ifdef _WIN32
+#include <windows.h>
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nShowCmd)
+{
+    ui::HistoricalMap historicalMap;
+
+    historicalMap.start();
+
+    return 0;
+}
+#else
 int main()
 {
     ui::HistoricalMap historicalMap;
@@ -8,3 +19,4 @@ int main()
 
     return 0;
 }
+#endif
