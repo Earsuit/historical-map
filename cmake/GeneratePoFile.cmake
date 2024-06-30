bd4e04195dca36637fe24bc32b21c07beeb180aa
earsuit <renyudicn@outlook.com>
2024-06-30 17:56:28 +0800

    rf: generate one single po file so we won't have same string different translation in different po files

--- cmake/GeneratePoFile.cmake
+++ cmake/GeneratePoFile.cmake
@@ -1,29 +1,15 @@
-if(NOT GETTEXT_FOUND)
-    message(FATAL_ERROR "gettext is not found!")
-endif()
-
-function(generatePoFile MODULE_NAME SOURCE_DIR SOURCES)
+function(generatePoFile SOURCES)
     file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/locale)
     file(MAKE_DIRECTORY ${CMAKE_SOURCE_DIR}/locale/${LOCALE})
-
-    # Construct the .pot file path
-    set(POT_FILE "${LOCALE_DIR}/${MODULE_NAME}.pot")
-    set(PO_FILE "${LOCALE_DIR}/${LOCALE}/${MODULE_NAME}.po")
-
-    set(FULL_PATH)
-    foreach(file IN LISTS SOURCES)
-        list(APPEND FULL_PATH "${SOURCE_DIR}/${file}")
-    endforeach()
 
     # Construct the xgettext command
     add_custom_command(
         OUTPUT POT_DUMMY
-        COMMAND ${GETTEXT_XGETTEXT_EXECUTABLE} -k__ -o ${POT_FILE} --from-code=UTF-8 ${FULL_PATH} --no-location
-        DEPENDS ${FULL_PATH}
-        COMMENT "Generating ${POT_FILE} from ${FULL_PATH}"
+        COMMAND ${GETTEXT_XGETTEXT_EXECUTABLE} -k__ -o ${POT_FILE} --from-code=UTF-8 ${SOURCES} --no-location --c++
+        COMMENT "Generating POT file"
     )
     # Create a custom target for generating the .pot file
-    add_custom_target(${MODULE_NAME}_pot ALL DEPENDS POT_DUMMY)
+    add_custom_target(translation_pot ALL DEPENDS POT_DUMMY)
 
     add_custom_command(
         OUTPUT PO_DUMMY
@@ -31,11 +17,13 @@ function(generatePoFile MODULE_NAME SOURCE_DIR SOURCES)
                 -DFILE_TO_CHECK=${PO_FILE}
                 -DPO_FILE=${PO_FILE}
                 -DPOT_FILE=${POT_FILE}
+                -DGETTEXT_MSGMERGE_EXECUTABLE=${GETTEXT_MSGMERGE_EXECUTABLE}
+                -DGETTEXT_MSGINIT_EXECUTABLE=${GETTEXT_MSGINIT_EXECUTABLE}
                 -P ${CMAKE_SOURCE_DIR}/cmake/PotFileToPoFile.cmake
-        DEPENDS ${MODULE_NAME}_pot
+        DEPENDS translation_pot
         COMMENT "Checking file existence and executing appropriate command"
     )
-    add_custom_target(${MODULE_NAME}_po ALL DEPENDS PO_DUMMY)
+    add_custom_target(translation_po ALL DEPENDS PO_DUMMY)
 
     # remove data so that only content change will trigger git tracking
 if (APPLE)
@@ -43,7 +31,7 @@ if (APPLE)
         OUTPUT REMOVE_DATE
         COMMAND sed -i '' '/POT-Creation-Date:/d' ${PO_FILE}    # macos needs an extra ''
         COMMAND sed -i '' '/PO-Revision-Date:/d' ${PO_FILE}
-        DEPENDS ${MODULE_NAME}_po
+        DEPENDS translation_po
         COMMENT "Remove DATE from ${PO_FILE}"
     )
 elseif(LINUX)
@@ -51,7 +39,7 @@ elseif(LINUX)
         OUTPUT REMOVE_DATE
         COMMAND sed -i '/POT-Creation-Date:/d' ${PO_FILE}
         COMMAND sed -i '/PO-Revision-Date:/d' ${PO_FILE}
-        DEPENDS ${MODULE_NAME}_po
+        DEPENDS translation_po
         COMMENT "Remove DATE from ${PO_FILE}"
     )
 else()
@@ -59,18 +47,18 @@ else()
     add_custom_command(
         OUTPUT REMOVE_DATE
         COMMAND pwsh -File ${CMAKE_SOURCE_DIR}/script/processPoFile.ps1 -poFile ${PO_FILE}
-        DEPENDS ${MODULE_NAME}_po
+        DEPENDS translation_po
         COMMENT "Remove DATE from ${PO_FILE}"
     )
 endif()
-    add_custom_target(REMOVE_DATE_${MODULE_NAME} ALL DEPENDS REMOVE_DATE)
+    add_custom_target(REMOVE_DATE_translation ALL DEPENDS REMOVE_DATE)
 
     # remove pot file
     add_custom_command(
         OUTPUT REMOVE_DUMMY # Use a dummy target
         COMMAND ${CMAKE_COMMAND} -E remove ${POT_FILE}
-        DEPENDS ${MODULE_NAME}_po
+        DEPENDS translation_po
         COMMENT "Removing file: ${POT_FILE}"
     )
-    add_custom_target(remove_${MODULE_NAME}_pot ALL DEPENDS REMOVE_DUMMY)
+    add_custom_target(remove_translation_pot ALL DEPENDS REMOVE_DUMMY)
 endfunction()
--- cmake/PotFileToPoFile.cmake
+++ cmake/PotFileToPoFile.cmake
@@ -1,8 +1,8 @@
 # Check if the file exists
 if(EXISTS "${FILE_TO_CHECK}")
-    message(STATUS "File ${FILE_TO_CHECK} exists, merging")
     execute_process(COMMAND ${GETTEXT_MSGMERGE_EXECUTABLE} --update ${PO_FILE} ${POT_FILE} --backup=off)
+    message(STATUS "File ${FILE_TO_CHECK} exists, merging")
 else()
-    message(STATUS "File ${FILE_TO_CHECK} does not exist, creating")
     execute_process(COMMAND ${GETTEXT_MSGINIT_EXECUTABLE} --input=${POT_FILE} --locale=zh_CN.UTF-8 --output=${PO_FILE} --no-translator)
+    message(STATUS "File ${FILE_TO_CHECK} does not exist, creating")
 endif()
